c71d1894f7c95a5f4c341173b91efcbf11db4087
Taylor Monacelli <taylormonacelli@gmail.com>
2024-10-27 22:43:25 -0700

    ok

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "github" {
 }
 
 locals {
-  repository_name = basename(path.cwd)
+  repository_name    = basename(path.cwd)
+  protected_branches = ["main", "master"]
 }
 
 data "github_repository" "repo" {
@@ -34,3 +35,31 @@ resource "github_repository" "repo" {
   archived               = false
 }
 
+resource "github_branch_protection" "protect_branches" {
+  for_each = toset(local.protected_branches)
+
+  repository_id = github_repository.repo.node_id
+  pattern       = each.value
+
+  enforce_admins                  = false
+  required_linear_history         = false
+  require_signed_commits          = false
+  require_conversation_resolution = false
+  allows_deletions                = true
+  allows_force_pushes             = true
+  required_status_checks {
+    strict   = false
+    contexts = ["Build & Test (ubuntu-latest)"]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = false
+    require_code_owner_reviews      = false
+    required_approving_review_count = 0
+  }
+}
+
+resource "github_actions_repository_permissions" "actions_permissions" {
+  repository      = github_repository.repo.name
+  allowed_actions = "all"
+  enabled         = true
+}
